52fcd78680af72c9650ab4fce13a7ba6b1b583e4
topilski <atopilski@gmail.com>
2020-12-15 07:05:23 -0500

    Hot fix

--- build_env.py
+++ build_env.py
@@ -110,8 +110,8 @@ class Debian(OperationSystem):
 
     def get_gst_repo_libs(self):
         return ['libglib2.0-dev', 'glib-networking', 'libgstreamer1.0-dev', 'libgstreamer-plugins-base1.0-dev',
-                'libgstreamer-plugins-good1.0-dev', 'libgstreamer-plugins-bad1.0-dev', 'libgstrtspserver-1.0-dev'
-                                                                                       'gstreamer1.0-tools',
+                'libgstreamer-plugins-good1.0-dev', 'libgstreamer-plugins-bad1.0-dev', 'libgstrtspserver-1.0-dev',
+                'gstreamer1.0-tools',
                 'gstreamer1.0-plugins-base', 'gstreamer1.0-plugins-good', 'gstreamer1.0-plugins-bad',
                 'gstreamer1.0-plugins-ugly', 'gstreamer1.0-libav', 'gstreamer1.0-rtsp']
 
