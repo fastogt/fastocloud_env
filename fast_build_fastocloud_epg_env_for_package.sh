ec697283615cedc17f4ce09c02efe3bfda6a4044
topilski <atopilski@gmail.com>
2020-12-17 04:05:58 -0500

    Build epg

---
+++ fast_build_fastocloud_epg_env_for_package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# exports
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+
+# variables
+USER=fastocloud
+
+# update system
+apt-get update
+apt-get install -y git python3-setuptools python3-pip --no-install-recommends
+
+# install pyfastogt
+git clone https://github.com/fastogt/pyfastogt
+cd pyfastogt
+python3 setup.py install
+cd ../
+rm -rf pyfastogt
+
+# build env for service
+./build_fastocloud_epg_env_from_repo.py
+
+# add user
+useradd -m -U -d /home/$USER $USER -s /bin/bash
