a295bc63f7e8f0052e7b177cfebf43244027621b
topilski <atopilski@gmail.com>
2020-12-21 19:51:14 +0300

    yum install env

--- fast_build_fastocloud_env_for_package.sh
+++ fast_build_fastocloud_env_for_package.sh
@@ -8,8 +8,29 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 USER=fastocloud
 
 # update system
-apt-get update
-apt-get install -y git python3-setuptools python3-pip --no-install-recommends
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if [ -n "$(command -v yum)" ]; then
+      yum update -y
+      yum install -y git python3-setuptools python3-pip
+    elif [ -n "$(command -v apt-get)" ]; then
+      apt-get update
+      apt-get install -y git python3-setuptools python3-pip --no-install-recommends
+    else
+:
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+:
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+:
+elif [[ "$OSTYPE" == "msys" ]]; then
+:
+elif [[ "$OSTYPE" == "win32" ]]; then
+:
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+:
+else
+:
+fi
 
 # install pyfastogt
 git clone https://github.com/fastogt/pyfastogt
