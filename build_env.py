06ac15bc114d031d567ea24aefa8e0008ef7d5e6
topilski <atopilski@gmail.com>
2022-01-19 00:43:14 -0600

    WPE port

--- build_env.py
+++ build_env.py
@@ -463,7 +463,7 @@ class BuildRequest(build_utils.BuildRequest):
         self._download_and_build_via_meson(url, compiler_flags)
 
     def build_wpe_webkit(self, version):
-        compiler_flags = []
+        compiler_flags = ['-DPORT=WPE']
         url = '{0}/wpewebkit-{1}.{2}'.format(WPE_WEBKIT_URL, version, WPE_WEBKIT_ARCH_EXT)
         self._download_and_build_via_cmake(url, compiler_flags)
 
