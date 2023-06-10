3ca083c82b0108bb1113265c242ff4ca61a9eeb7
Alexandr Topilski <atopilski@gmail.com>
2023-06-10 06:18:02 +0300

    Update build_env.py

--- build_env.py
+++ build_env.py
@@ -488,7 +488,7 @@ class BuildRequest(build_utils.BuildRequest):
 
     def build_wpe_webkit(self, version):
         compiler_flags = ['-DPORT=WPE', '-DUSE_SOUP2=ON', '-DENABLE_ACCESSIBILITY=OFF', '-DUSE_OPENJPEG=OFF',
-                          '-DUSE_WOFF2=OFF', '-DUSE_LCMS=OFF', '-DENABLE_BUBBLEWRAP_SANDBOX=OFF']
+                          '-DUSE_WOFF2=OFF', '-DUSE_LCMS=OFF', '-DUSE_AVIF=OFF', '-DENABLE_BUBBLEWRAP_SANDBOX=OFF', '-DENABLE_INTROSPECTION=OFF']
         url = '{0}/wpewebkit-{1}.{2}'.format(WPE_WEBKIT_URL, version, WPE_WEBKIT_ARCH_EXT)
         self._download_and_build_via_cmake(url, compiler_flags)
 
