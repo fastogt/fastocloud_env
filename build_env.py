20781678660d9c88c3c3c6bf60ee86cdf32266d9
Alexandr Topilski <atopilski@gmail.com>
2024-06-23 08:31:31 +0300

    Update build_env.py

--- build_env.py
+++ build_env.py
@@ -636,7 +636,7 @@ def str2bool(v):
 if __name__ == "__main__":
     meson_default_version = '1.1.0'
     srt_default_version = '1.5.3'
-    gstreamer_default_version = '1.24.4'
+    gstreamer_default_version = '1.24.5'
     # https://wpewebkit.org/release/
     wpe_version = '1.14.2'
     wpe_backend_version = '1.14.2'
